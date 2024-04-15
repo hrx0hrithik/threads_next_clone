b962d51fc3d5cede0e5f8eaf8b7429d72b0ee62d
Hrithik Gupta <hrithikgupta52@gmail.com>
2024-04-16 02:13:50 +0530

    home testing

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -6,11 +6,11 @@ import { redirect } from "next/navigation";
  
 async function Home() {
   const result = await fetchPost(1, 30)
-  const user = await currentUser() || null
+  const user = await currentUser();
 
-  // if (!user) return null;
+  if (!user) return null;
 
-  const userInfo = await fetchUser(user?.id);
+  const userInfo = await fetchUser(user.id);
   if (!userInfo?.onboarded) redirect("/onboarding");
 
   return (
